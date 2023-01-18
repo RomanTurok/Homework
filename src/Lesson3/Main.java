895fb3ed2937f8da665db66e54475b6cc0895579
Roman Turok <romaturokpw@ukr.net>
2023-01-18 21:27:37 +0000

    Commit

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -1,4 +1,27 @@
 package Lesson3;
 
 public class Main {
+    public static void main(String[] args) {
+        int b = 2;
+        int a = 33;
+        double d = 15.8;
+        double c = 33.5;
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(a-b);
+        System.out.println(b+a);
+        System.out.println(a*(d+c));
+        System.out.println((a*d)+(b*c));
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
