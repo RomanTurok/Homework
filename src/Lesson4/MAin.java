258dfe79a167847c2b75f3f9f0f24c60aa62f00a
Roman Turok <romaturokpw@ukr.net>
2023-01-23 17:00:35 +0000

    lesson 3 check

---
+++ src/Lesson4/MAin.java
@@ -0,0 +1,21 @@
+package Lesson4;
+
+public class MAin {
+    public static void main(String[] args) {
+
+        String str = "Hello Everybody";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+        System.out.println(str.charAt(0));
+
+        int lenghts = str.length();
+        int lastIndex = lenghts - 1;
+        System.out.println(str.charAt(lastIndex));
+
+
+
+
+
+    }
+}
