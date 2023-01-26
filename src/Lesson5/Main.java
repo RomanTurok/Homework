2825a3b2847204778b8af5be205e892821a22de0
Roman Turok <romaturokpw@ukr.net>
2023-01-26 17:01:56 +0000

    Commit

---
+++ src/Lesson5/Main.java
@@ -0,0 +1,28 @@
+package Lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 12;
+
+        if (number > 5) {
+            System.out.println(number + " > 5");
+        }
+        if (number < 5) {
+            System.out.println(number + " < 5");
+        }
+
+
+        if (number > 10) {
+            System.out.println(number + " > 10");
+        } else {
+            System.out.println(number + " < 10");
+        }
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
+        }
+    }
+}
