bc6285d22858488306cb856538e7c15418e82069
Roman Turok <romaturokpw@ukr.net>
2023-02-11 21:10:57 +0000

    Commit

--- src/Lesson8/Main.java
+++ src/Lesson8/Main.java
@@ -1,31 +1,4 @@
 package Lesson8;
 
 public class Main {
-    public static void main(String[] args) {
-
-        int a = 4;
-        a = cubeNumber(a);
-        System.out.println(a);
-
-        printNumberOfStars(25);
-        printSomeSymbolTimes(20, " £ ");
-
-    }
-    static int cubeNumber (int cube){
-        return  cube * cube * cube;
-    }
-
-    static void printNumberOfStars (int star){
-        for (int i = 0; i < star; i++) {
-            System.out.print( " * ");
-        }
-        System.out.println();
-    }
-    static void printSomeSymbolTimes(int number, String symbol){
-        for (int i = 0; i < number; i++) {
-            System.out.print(symbol);
-        }
-        System.out.println();
-    }
-
 }
