d0c200dcf2ae2b1e68dca79f30211e013d20fab2
Roman Turok <romaturokpw@ukr.net>
2023-03-09 18:25:50 +0000

    Commit

--- src/Lesson17/Main.java
+++ src/Lesson17/Main.java
@@ -2,7 +2,12 @@ package Lesson17;
 
 public class Main {
     public static void main(String[] args) {
+        DayOfWeek dayOfWeek = DayOfWeek.FRIDAY;
+        System.out.println(dayOfWeek);
 
+        for (DayOfWeek value : DayOfWeek.values()){
+            System.out.println(value + " " + value.getNumber());
+        }
 
     }
 }
