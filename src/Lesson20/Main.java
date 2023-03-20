49fc1cf97d745ae046e877a8f333dcdefcf8010a
Roman Turok <romaturokpw@ukr.net>
2023-03-20 20:47:57 +0000

    Commit

---
+++ src/Lesson20/Main.java
@@ -0,0 +1,32 @@
+package Lesson20;
+
+import java.io.Console;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.SocketHandler;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String, String> word1 = new HashMap<>();
+        word1.put("Сніг" , "Eng - Snow; Ger - Schee; Jap - 雪;");
+        word1.put("Вода" , "Eng - Water; Ger - Wasser; Jap - 水;");
+        word1.put("Автомобіль" , "Eng - Car; Ger - Machine; Jap - マシーン;");
+        word1.put("Хліб" , "Eng - Bread ; Ger - Brot; Jap - パン;");
+        word1.put("Сумка" , "Eng - Bag; Ger - Tasche; Jap - バッグ;");
+        word1.put("Гроші" , "Eng - Money; Ger - Geld; Jap - お金;");
+        word1.put("Куртка" , "Eng - Jacket; Ger - Jacke; Jap - ジャケット;");
+        word1.put("Цибуля" , "Eng - Onion; Ger - Zwiebel; Jap - タマネギ;");
+        word1.put("Вітер" , "Eng - Wind; Ger - Wind; Jap - 風;");
+        word1.put("Стіл" , "Eng - Table; Ger - Tisch; Jap - テーブル;");
+
+
+        System.out.println(word1.get("Сніг"));
+
+
+
+
+    }
+
+}
