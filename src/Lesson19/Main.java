3ad4768bd9bc2f5566427b1e493502516436e804
Roman Turok <romaturokpw@ukr.net>
2023-03-23 18:37:50 +0000

    Commit

--- src/Lesson19/Main.java
+++ src/Lesson19/Main.java
@@ -3,6 +3,13 @@ package Lesson19;
 public class Main {
     public static void main(String[] args) {
 
+        Collections collections = new Collections();
+
+        System.out.println(collections.getHashSetCollection());
+        System.out.println(collections.getLinkedHashSetCollection());
+        System.out.println(collections.getTreeSetCollection());
+
+
 
     }
 }
